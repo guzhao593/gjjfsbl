c77243b81575e9cb9adf65c072f10b5de38d051c
zhaojj6 <guzhao593@163.com>
2020-02-04 15:35:44 +0800

    完成我的推广展示

--- miniprogram/pages/user/index.js
+++ miniprogram/pages/user/index.js
@@ -1,3 +1,4 @@
+import drawQrcode from '../../utils/weapp.qrcode.js'
 const app = getApp();
 const db = wx.cloud.database();
 
@@ -49,6 +50,80 @@ Page({
       userInfo: globalData.userInfo
     })
     this.getTabBar().init();
+  },
+
+  draw () {
+    drawQrcode({
+      width: 160,
+      height: 160,
+      x: 20,
+      y: 20,
+      canvasId: 'qrcode',
+      typeNumber: 10,
+      text: this.generateQrcodeUrl(app.globalData.userInfo),
+      image: {
+        imageResource: '../../images/logo.png',
+        dx: 70,
+        dy: 70,
+        dWidth: 60,
+        dHeight: 60
+      },
+      callback(e) {
+        console.log('e: ', e)
+      }
+    })
+  },
+
+  generateQrcodeUrl (data) {
+    return `http://gjjfc666666.gz01.bdysite.com/gjjfsbl?serviceNetworkCode=${data.serviceNetworkCode}&serviceProviderCode=${data.serviceProviderCode}`
+  },
+
+  handleShowQrcode () {
+    this.draw()
+    this.setData({
+      showQrcode: true
+    })
+  },
+
+  download () {
+    // 导出图片
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 300,
+      height: 300,
+      destWidth: 300,
+      destHeight: 300,
+      canvasId: 'qrcode',
+      success(res) {
+        let tempFilePath = res.tempFilePath
+
+        // 保存到相册
+        wx.saveImageToPhotosAlbum({
+          filePath: tempFilePath,
+          success: function (res) {
+            wx.showToast({
+              title: '保存成功',
+              icon: 'success',
+              duration: 2000
+            })
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '保存失败',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        })
+      }
+    })
+  },
+
+  confirm () {
+    this.setData({
+      showQrcode: false
+    })
   },
 
   handleLoginType ({ target }) {
